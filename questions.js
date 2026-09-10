86f5fa5e69166f660dd0d986c878dbecfa86fae4
khainguyenxuan86-cyber <khai.nguyenxuan86@gmail.com>
2026-09-10 11:37:05 +0700

    Initialize gameData with quiz questions
    Added a quiz data structure with questions, images, and answers.

---
+++ questions.js
@@ -0,0 +1,56 @@
+const gameData = {
+
+  title: "🎮 Movers Unit 4 Quiz",
+
+  questions: [
+
+    {
+      question: "What do you use to write?",
+
+      image: "images/q1.jpg",
+
+      answers: [
+        "A pencil",
+        "A banana",
+        "A shoe",
+        "A ball"
+      ],
+
+      correct: 0
+    },
+
+
+    {
+      question: "Which animal can fly?",
+
+      image: "images/q2.jpg",
+
+      answers: [
+        "Dog",
+        "Cat",
+        "Bird",
+        "Fish"
+      ],
+
+      correct: 2
+    },
+
+
+    {
+      question: "Which one is a fruit?",
+
+      image: "images/q3.jpg",
+
+      answers: [
+        "Apple",
+        "Chair",
+        "Book",
+        "Car"
+      ],
+
+      correct: 0
+    }
+
+  ]
+
+};
